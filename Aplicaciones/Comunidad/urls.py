fa7b612108e7f423839b04e0e753a99ff05f7589
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-07-31 12:05:11 -0500

    Configuracion del archivo urls.py de Comunidades

---
+++ Aplicaciones/Comunidad/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listaComunidad, name='lista_comunidad'),
+    path('nuevo/', views.nuevoComunidad, name='nuevo_comunidad'),
+    path('guardar/', views.guardarComunidad, name='guardar_comunidad'),
+    path('editar/<int:id>/', views.editarComunidad, name='editar_comunidad'),
+    path('procesar/', views.procesarEdicionComunidad, name='procesar_edicion_comunidad'),
+    path('eliminar/<int:id>/', views.eliminarComunidad, name='eliminar_comunidad'),
+]
--- Reforestacion/urls.py
+++ Reforestacion/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('especie/', include('Aplicaciones.Especie.urls')),
+    path('comunidad/', include('Aplicaciones.Comunidad.urls')),
 ]
